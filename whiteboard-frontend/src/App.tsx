b54feaa3ded48bbe21b7938189007f569cf5df97
Raj <ranadkar@ucsc.edu>
2026-07-13 08:14:57 -0700

    cleanup

--- whiteboard-frontend/src/App.tsx
+++ whiteboard-frontend/src/App.tsx
@@ -4,16 +4,9 @@ import Whiteboard from './components/Whiteboard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>BOARD</h1>
-        <p>draw in real time :)</p>
-      </header>
       <main>
         <Whiteboard />
       </main>
-      <footer>
-        <p>Open this page in multiple windows and see what happens</p>
-      </footer>
     </div>
   );
 }
